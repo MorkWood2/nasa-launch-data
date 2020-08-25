f5121e140f82b36424509cf05a5155643cba840b
Mark Wild <mark.wild189@gmail.com>
2020-08-25 12:43:19 -0700

    add async middleware

--- mod.ts
+++ mod.ts
@@ -2,9 +2,23 @@ import { Application } from "https://deno.land/x/oak@v6.0.2/mod.ts";
 
 const app = new Application();
 const PORT = 8000;
+//next() async function that triggers next piece of middleware is called
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`);
+});
 
-app.use((ctx) => {
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const delta = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${delta} ms`);
+});
+
+app.use(async (ctx, next) => {
   ctx.response.body = "hello world";
+  await next();
 });
 
 if (import.meta.main) {
