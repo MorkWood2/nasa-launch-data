c24c36e392c3b937c05b61624704988c6e6570d6
Mark Wild <mark.wild189@gmail.com>
2020-08-26 21:24:52 -0700

    add planets.ts and csv file

--- mod.ts
+++ mod.ts
@@ -1,5 +1,7 @@
 import { Application, send } from "https://deno.land/x/oak@v6.0.2/mod.ts";
 
+import api from "./api.ts";
+
 const app = new Application();
 const PORT = 8000;
 //next() async function that triggers next piece of middleware is called
@@ -16,6 +18,9 @@ app.use(async (ctx, next) => {
   ctx.response.headers.set("X-Response-Time", `${delta} ms`);
 });
 
+app.use(api.routes());
+app.use(api.allowedMethods());
+//static file middleware
 app.use(async (ctx) => {
   const filePath = ctx.request.url.pathname;
   const fileWhitelist = [
@@ -24,14 +29,11 @@ app.use(async (ctx) => {
     "/stylesheets/style.css",
     "/images/favicon.png",
   ];
-  await send(ctx, filePath, {
-    root: `${Deno.cwd()}/public`,
-  });
-});
-
-app.use(async (ctx, next) => {
-  ctx.response.body = "hello world";
-  await next();
+  if (fileWhitelist.includes(filePath)) {
+    await send(ctx, filePath, {
+      root: `${Deno.cwd()}/public`,
+    });
+  }
 });
 
 if (import.meta.main) {
---
+++ models/planets.ts
@@ -0,0 +1,62 @@
+//module allows filePath to be read by windows and mac
+import { join } from "https://deno.land/std/path/mod.ts";
+// BufReader implements buffering for a Reader object
+import { BufReader } from "https://deno.land/std/io/bufio.ts";
+//parse csv function
+import { parse } from "https://deno.land/std/encoding/csv.ts";
+
+import * as _ from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
+
+//define typescript interface
+
+//each planet object is a string
+type Planet = Record<string, string>;
+
+let planets: Array<Planet>;
+
+async function loadPlanetsData() {
+  //           join('currentDIR', 'fileName')
+  const path = join("data", "kepler_exoplanets_nasa.csv");
+
+  const file = await Deno.open(path);
+  const bufReader = new BufReader(file);
+  const result = await parse(bufReader, {
+    // parse header and commit
+    header: true,
+    comment: "#",
+  });
+  //Deno.close to prevent memory leak
+  Deno.close(file.rid);
+
+  //                    type assertion
+  const planets = (result as Array<Planet>).filter((planet) => {
+    const plantaryRadius = Number(planet["koi_prad"]);
+    const stellarMass = Number(planet["koi_smass"]);
+    const stellarRadius = Number(planet["koi_srad"]);
+
+    return planet["koi_disposition"] === "CONFIRMED" &&
+      plantaryRadius > 0.5 && plantaryRadius < 1.5 &&
+      stellarMass > 0.78 && stellarMass < 1.04 &&
+      stellarRadius > 0.99 && stellarRadius < 1.01;
+  });
+  //_.pick() lodash Creates an object composed of the picked object properties.
+
+  return planets.map((planet) => {
+    return _.pick(planet, [
+      "koi_prad",
+      "koi_smass",
+      "koi_srad",
+      "kepler_name",
+      "koi_count",
+      "koi_steff",
+    ]);
+  });
+}
+
+planets = await loadPlanetsData();
+
+console.log(`${planets.length} habitable planets found!`);
+
+export function getAllPlanets() {
+  return planets;
+}
